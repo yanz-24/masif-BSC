1b251e9dc2fa127b38b37b83ea8ff7251e5cf920
yazh-24 <zhangyy9824@gmail.com>
2025-03-27 21:49:11 +0100

    debug

--- data/masif_bsc/data_prepare_one.sh
+++ data/masif_bsc/data_prepare_one.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
-masif_root=$(git rev-parse --show-toplevel)
-masif_source=$masif_root/source/
+echo $1, $2, $3
+masif_root=/home/yanyuan.zhang/project/masif-toughm1/masif-BSC
+masif_source=$masif_root/source
+wd=$masif_root/data/masif_bsc
+cd $wd
 export PYTHONPATH=$PYTHONPATH:$masif_source
 
-if [ "$1" == "--file" ] || [ "$1" == "-f" ]
-then
-	echo "Running masif site on user's customized file $2"
-	ID_CHAIN=$3
-	PDB_ID=$(echo $ID_CHAIN| cut -d"_" -f1)
-	CHAIN=$(echo $ID_CHAIN| cut -d"_" -f2)
-	FILENAME=$2
-	mkdir -p data_preparation/00-raw_pdbs/
-	cp $FILENAME data_preparation/00-raw_pdbs/$PDB_ID\.pdb
+if [ "$1" == "--file" ] || [ "$1" == "-f" ]; then
+	# check if input file provided
+	if [ -z "$2" ]; then
+		echo "Usage: $0 [-f|--file <file_path>] <ID_CHAIN>"
+		exit 1
+	else
+		echo "Running masif on user's customized file $2"
+		ID_CHAIN=$3
+		PDBID=$(echo $ID_CHAIN| cut -d"_" -f1)
+		CHAIN=$(echo $ID_CHAIN| cut -d"_" -f2)
+		FILENAME=$2
+		output_dir=$wd/data_preparation/00-raw_pdbs/
+		mkdir -p $output_dir
+		cp $FILENAME $output_dir/$PDBID\.pdb
+	fi
+	
 else
 	# download from PDB
 	echo "Downloading $2 from PDB"
-	$ID_CHAIN=$1
-	PDB_ID=$(echo $PPI_PAIR_ID| cut -d"_" -f1)
-	CHAIN=$(echo $PPI_PAIR_ID| cut -d"_" -f2)
+	ID_CHAIN=$1
+	PDBID=$(echo $ID_CHAIN| cut -d"_" -f1)
+	CHAIN=$(echo $ID_CHAIN| cut -d"_" -f2)
 	python -W ignore $masif_source/data_preparation/00-pdb_download.py $ID_CHAIN
 fi
 
-python -W ignore $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN
+echo "01 Extracting and triangulating $ID_CHAIN"
+python -W ignore $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDBID\_$CHAIN
+
+echo "04 Computing descriptors for $ID_CHAIN"
 python $masif_source/data_preparation/04-masif_precompute.py masif_site $ID_CHAIN
