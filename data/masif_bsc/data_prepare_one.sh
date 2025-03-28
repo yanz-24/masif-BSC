a39018a042916a85c73773652db8122124244eb0
yazh-24 <zhangyy9824@gmail.com>
2025-03-28 10:52:49 +0100

    change variable

--- data/masif_bsc/data_prepare_one.sh
+++ data/masif_bsc/data_prepare_one.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 masif_root=/home/yanyuan.zhang/project/masif-toughm1/masif-BSC
 masif_source=$masif_root/source
+masif_matlab=$masif_root/source/matlab_libs/
+export PYTHONPATH=$PYTHONPATH:$masif_source
+export masif_matlab
+
 wd=$masif_root/data/masif_bsc
 cd $wd
-export PYTHONPATH=$PYTHONPATH:$masif_source
+
+
 
 if [ "$1" == "--file" ] || [ "$1" == "-f" ]; then
 	# check if input file provided
 	if [ -z "$2" ]; then
-		echo "Usage: $0 [-f|--file <file_path>] <ID_CHAIN>"
+		echo "Usage: $0 [-f|--file <file_path>] <PDBID_CHAIN>"
 		exit 1
 	else
 		echo "Running masif on user's customized file $2"
-		ID_CHAIN=$3
-		PDBID=$(echo $ID_CHAIN| cut -d"_" -f1)
-		CHAIN=$(echo $ID_CHAIN| cut -d"_" -f2)
+		PDBID_CHAIN=$3
+		PDBID=$(echo $PDBID_CHAIN| cut -d"_" -f1)
+		CHAIN=$(echo $PDBID_CHAIN| cut -d"_" -f2)
 		FILENAME=$2
 		output_dir=$wd/data_preparation/00-raw_pdbs/
 		mkdir -p $output_dir
@@ -24,14 +29,14 @@ if [ "$1" == "--file" ] || [ "$1" == "-f" ]; then
 else
 	# download from PDB
 	echo "Downloading $2 from PDB"
-	ID_CHAIN=$1
-	PDBID=$(echo $ID_CHAIN| cut -d"_" -f1)
-	CHAIN=$(echo $ID_CHAIN| cut -d"_" -f2)
-	python -W ignore $masif_source/data_preparation/00-pdb_download.py $ID_CHAIN
+	PDBID_CHAIN=$1
+	PDBID=$(echo $PDBID_CHAIN| cut -d"_" -f1)
+	CHAIN=$(echo $PDBID_CHAIN| cut -d"_" -f2)
+	python -W ignore $masif_source/data_preparation/00-pdb_download.py $PDBID_CHAIN
 fi
 
-echo "01 Extracting and triangulating $ID_CHAIN"
+echo "01 Extracting and triangulating $PDBID_CHAIN"
 python -W ignore $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDBID\_$CHAIN
 
-echo "04 Computing descriptors for $ID_CHAIN"
-python $masif_source/data_preparation/04-masif_precompute.py masif_site $ID_CHAIN
+echo "04 Computing descriptors for $PDBID_CHAIN"
+python $masif_source/data_preparation/04-masif_precompute.py masif_site $PDBID_CHAIN
