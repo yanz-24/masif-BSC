37096cab515c6ab694407a42723b816140515842
yazh-24 <zhangyy9824@gmail.com>
2025-03-28 13:51:57 +0100

    check if pdb has been processed

--- data/masif_bsc/data_prepare_one.sh
+++ data/masif_bsc/data_prepare_one.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 masif_root=/home/yanyuan.zhang/project/masif-toughm1/masif-BSC
 masif_source=$masif_root/source
-masif_matlab=$masif_root/source/matlab_libs/
+masif_matlab=$masif_root/source/matlab_libs
 export PYTHONPATH=$PYTHONPATH:$masif_source
 export masif_matlab
 
@@ -9,34 +9,51 @@ wd=$masif_root/data/masif_bsc
 cd $wd
 
 
-
 if [ "$1" == "--file" ] || [ "$1" == "-f" ]; then
 	# check if input file provided
 	if [ -z "$2" ]; then
-		echo "Usage: $0 [-f|--file <file_path>] <PDBID_CHAIN>"
+		printf "[ERROR] Usage: %s [-f|--file <file_path>] <PDBID_CHAIN>\n" "$0"
 		exit 1
-	else
-		echo "Running masif on user's customized file $2"
-		PDBID_CHAIN=$3
-		PDBID=$(echo $PDBID_CHAIN| cut -d"_" -f1)
-		CHAIN=$(echo $PDBID_CHAIN| cut -d"_" -f2)
-		FILENAME=$2
-		output_dir=$wd/data_preparation/00-raw_pdbs/
-		mkdir -p $output_dir
-		cp $FILENAME $output_dir/$PDBID\.pdb
 	fi
+	
+	printf "[INFO] Running masif on user's customized file: %s\n" "$2"
+	FILENAME=$2
+	PDBID_CHAIN=$3
+
+	# check if the file has been precomputed
+	output_dir_04=$wd/data_preparation/04a-precomputation_9A/precomputation
+	output_04=$output_dir_04/$PDBID_CHAIN
+
+	echo $output_04
+	if [ -e "$output_04" ]; then
+		printf "[INFO] Precomputed file %s already exists. Skipping precomputation.\n" "$output_04"
+		exit 0
+	fi
+	
+	PDBID=$(echo $PDBID_CHAIN| cut -d"_" -f1)
+	CHAIN=$(echo $PDBID_CHAIN| cut -d"_" -f2)
+
+	if [ ! -f "$FILENAME" ]; then
+		printf "[ERROR] File %s does not exist.\n" "$FILENAME"
+		exit 1
+	fi
+	
+	output_dir_00=$wd/data_preparation/00-raw_pdbs
+	mkdir -p $output_dir_00
+	cp $FILENAME $output_dir_00/$PDBID\.pdb
+	
 	
 else
 	# download from PDB
-	echo "Downloading $2 from PDB"
+	printf "[INFO] Downloading %s from PDB\n" "$1"
 	PDBID_CHAIN=$1
 	PDBID=$(echo $PDBID_CHAIN| cut -d"_" -f1)
 	CHAIN=$(echo $PDBID_CHAIN| cut -d"_" -f2)
 	python -W ignore $masif_source/data_preparation/00-pdb_download.py $PDBID_CHAIN
 fi
 
-echo "01 Extracting and triangulating $PDBID_CHAIN"
+printf "[INFO] 01 Extracting and triangulating %s\n" "$PDBID_CHAIN"
 python -W ignore $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDBID\_$CHAIN
 
-echo "04 Computing descriptors for $PDBID_CHAIN"
+printf "[INFO] 04 Computing descriptors for %s\n" "$PDBID_CHAIN"
 python $masif_source/data_preparation/04-masif_precompute.py masif_site $PDBID_CHAIN
