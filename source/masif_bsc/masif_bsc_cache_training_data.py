1397f1daf78b51590ada5a558dd9888b57e7b742
yazh-24 <zhangyy9824@gmail.com>
2025-04-11 08:31:09 +0200

    add validation set

--- source/masif_bsc/masif_bsc_cache_training_data.py
+++ source/masif_bsc/masif_bsc_cache_training_data.py
@@ -10,18 +10,21 @@ from default_config.masif_opts import masif_opts
 masif_bsc_cache_training_data.py: TODO
 """
 
+# get parameters
 params = masif_opts['ppi_search']
 parent_in_dir = params['masif_precomputation_dir']
+val_range = 1 - params['range_val_samples']
 
 # read training and testing list
 train_df = pd.read_csv(params['training_list'])  # should contains：p1, p2, label (0/1)
 test_df = pd.read_csv(params['testing_list'])
 
+# initialize
 binder_rho_wrt_center, binder_theta_wrt_center, binder_input_feat, binder_mask = [], [], [], []
 pos_rho_wrt_center, pos_theta_wrt_center, pos_input_feat, pos_mask = [], [], [], []
 neg_rho_wrt_center, neg_theta_wrt_center, neg_input_feat, neg_mask = [], [], [], []
 
-training_idx, test_idx = [], []
+training_idx, val_idex, test_idx = [], [], []
 pos_names, neg_names = [], []
 
 idx_count = 0
@@ -70,6 +73,8 @@ def load_sample(p1_id, p2_id, label, split):
             training_idx.append(idx_count)
         elif split == 'test':
             test_idx.append(idx_count)
+        elif split == 'val':
+            val_idex.append(idx_count)
 
         idx_count += 1
 
@@ -77,12 +82,24 @@ def load_sample(p1_id, p2_id, label, split):
         print(f"Error loading {p1_id}, {p2_id}: {e}")
 
 
-# deal with trainig and testing set
-for _, row in train_df[0:3].iterrows():
+# Shuffle training data and assign val/train
+np.random.seed(0)
+train_df_shuffled = train_df.sample(frac=1).reset_index(drop=True)
+val_cutoff = int(val_range * len(train_df_shuffled))
+
+val_rows = train_df_shuffled.iloc[:val_cutoff]
+train_rows = train_df_shuffled.iloc[val_cutoff:]
+
+# deal with trainig, validation and testing set
+for _, row in train_rows[0:2].iterrows():
     load_sample(row['p1'], row['p2'], row['label'], 'train')
 
-for _, row in test_df[0:3].iterrows():
+for _, row in val_rows[0:2].iterrows():
+    load_sample(row['p1'], row['p2'], row['label'], 'val')
+
+for _, row in test_df[0:2].iterrows():
     load_sample(row['p1'], row['p2'], row['label'], 'test')
+
 
 # save in cache
 if not os.path.exists(params['cache_dir']):
@@ -105,16 +122,18 @@ save_npy('pos_theta_wrt_center', np.concatenate(pos_theta_wrt_center, axis=0))
 save_npy('pos_input_feat', np.concatenate(pos_input_feat, axis=0))
 save_npy('pos_mask', np.concatenate(pos_mask, axis=0))
 save_npy('pos_names', pos_names)
+save_npy('pos_training_idx', training_idx)
+save_npy('pos_test_idx', test_idx)
+save_npy('pos_val_idx', val_idex)
 
 save_npy('neg_rho_wrt_center', np.concatenate(neg_rho_wrt_center, axis=0))
 save_npy('neg_theta_wrt_center', np.concatenate(neg_theta_wrt_center, axis=0))
 save_npy('neg_input_feat', np.concatenate(neg_input_feat, axis=0))
 save_npy('neg_mask', np.concatenate(neg_mask, axis=0))
 save_npy('neg_names', neg_names)
-
-save_npy('pos_training_idx', training_idx)
-save_npy('pos_test_idx', test_idx)
 save_npy('neg_training_idx', training_idx)
 save_npy('neg_test_idx', test_idx)
+save_npy('neg_val_idx', val_idex)
+
 
 print(f"number of positives: {len(pos_names)} | negatives: {len(neg_names)}")
