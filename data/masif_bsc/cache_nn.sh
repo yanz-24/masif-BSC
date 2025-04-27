ae2af2b21828bcea02cfc23e36ac2ca95e8a90e3
yazh-24 <zhangyy9824@gmail.com>
2025-04-27 18:19:42 +0200

    add commend to enter working directory

--- data/masif_bsc/cache_nn.sh
+++ data/masif_bsc/cache_nn.sh
@@ -3,4 +3,7 @@ masif_source=$masif_root/source
 masif_data=$masif_root/data
 masif_matlab=$masif_root/source/matlab_libs
 export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_ppi_search/
+
+wd=$masif_root/data/masif_bsc
+cd $wd
 python3 $masif_source/masif_bsc/masif_bsc_cache_training_data.py
