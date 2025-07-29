eedaa7d3a71e2b6e919cf9ce0abec49b1afeb1e7
yazh-24 <zhangyy9824@gmail.com>
2025-07-29 16:51:37 +0200

    move parameters here

--- source/default_config/masif_opts.py
+++ source/default_config/masif_opts.py
@@ -25,14 +25,14 @@ masif_opts["radius"] = 12.0
 masif_opts["ppi_search"] = {}
 masif_opts["ppi_search"]["training_list"] = "lists/training.txt"
 masif_opts["ppi_search"]["testing_list"] = "lists/testing.txt"
-masif_opts["ppi_search"]["training_triplets"] = "lists/training_list.txt"
-masif_opts["ppi_search"]["validation_triplets"] = "lists/validation_list.txt"
-masif_opts["ppi_search"]["testing_triplets"] = "lists/testing_list.txt"
+masif_opts["ppi_search"]["training_triplets"] = "lists/training_list100.txt"
+masif_opts["ppi_search"]["validation_triplets"] = "lists/validation_list100.txt"
+masif_opts["ppi_search"]["testing_triplets"] = "lists/testing_list100.txt"
 masif_opts["ppi_search"]["max_shape_size"] = 200
 masif_opts["ppi_search"]["max_distance"] = 12.0  # Radius for the neural network.
 masif_opts["ppi_search"][
     "masif_precomputation_dir"
-] ="data_preparation/04a-precomputation_9A/precomputation/" # old: "data_preparation/04b-precomputation_12A/precomputation/" 
+] ="data_preparation/04b-precomputation_12A/precomputation/" # old: "data_preparation/04b-precomputation_12A/precomputation/" 
 masif_opts["ppi_search"]["feat_mask"] = [1.0] * 5
 masif_opts["ppi_search"]["max_sc_filt"] = 1.0
 masif_opts["ppi_search"]["min_sc_filt"] = 0.5
@@ -84,3 +84,37 @@ masif_opts["ligand"]["costfun"] = "dprime"
 masif_opts["ligand"]["model_dir"] = "nn_models/all_feat/"
 masif_opts["ligand"]["test_set_out_dir"] = "test_set_predictions/"
 
+# Model parameters
+MODEL_PARAMS = {
+    'max_rho': 12.0,            # Maximum radial distance
+    'n_thetas': 16,             # Number of angular bins
+    'n_rhos': 5,                # Number of radial bins
+    'n_gamma': 1.0,             # Gamma parameter
+    'n_rotations': 16,          # Number of rotations for data augmentation
+    'idx_gpu': "/device:GPU:0", # GPU device (use "/device:CPU:0" for CPU)
+    'feat_mask': [1.0, 1.0, 1.0, 1.0, 1.0]  # Feature mask (which features to use)
+}
+
+# Training parameters
+TRAINING_PARAMS = {
+    'learning_rate': 1e-3,
+    'keep_prob': 0.8,                   # Dropout keep probability
+    'n_epochs': 3,                      # Number of epochs to train
+    'batch_size': 1,                    # Number of quadruplets [binder, pos, neg, neg2] in one batch. 
+    'num_iterations' : 1000,            # Total number of training iterations
+    'print_every': 10,                  # Print loss every N iterations
+    'val_every': 10,                    # Number of iterations to validate the model, i.e. evaluate the model on the test set every N iterations
+    'cache_every' : 10,                 # Cache the data every N iterations
+    'early_stopping': True,             # Whether to use early stopping
+    'early_stopping_patience': None,      # Number of iterations to wait for improvement before stopping training
+    'early_stopping_delta': 0.01,       # Minimum change to qualify as an improvement
+    'early_stopping_metric': 'loss',    # Metric to monitor for early stopping, 'roc_auc' or 'loss'
+}
+
+# Data parameters
+DATA_PARAMS = {
+    'data_path': '/Users/yanyz/Ratar/masif-BSC/data/masif_bsc/nn_models/sc05/cache',  # TODO
+    'patches_per_protein': 32,  # Number of patches per protein
+    'triplets': '/Users/yanyz/Ratar/masif-BSC/source/masif_bsc/training_list100.txt',
+    'models': '/Users/yanyz/Ratar/masif-BSC/data/masif_bsc/models' 
+}
